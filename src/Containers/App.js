f92f7b0e4086763afcb15e50191651fc873b8f0d
brad heath <heathbar31@brads-mbp.lan>
2023-05-01 17:38:35 -0500

    update with error boundry

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 // import { robots } from './robots';
 import './App.css';
+import ErrorBoundry from '../Components/ErrorBoundry';
 import Scroll from '../Components/Scroll';
 
 //Since app is the core of application, make it a class and a react component, and then make constructor declaring two variables as states because they change state. 
@@ -33,8 +34,8 @@ class App extends React.Component {
         const roboFilter = robots.filter(roboSearch => {
             return roboSearch.name.toLowerCase().includes(searchfield.toLowerCase());  //set robot name to lowercase, and filter by searchfield data
         })
-        return !robots.length ? //if no robots
-            <h1>Loading Robots</h1> :
+        return !robots.length ? 
+            <h1>Loading Robots</h1> : //if no robots
 
         //return html data and searchbox, scroll, and cardlist component. Cardlist is a child class of scroll. 
        (
@@ -42,7 +43,9 @@ class App extends React.Component {
               <h1 className='f1'>RoboFriends</h1>
               <SearchBox searchChange={this.onSearchChange}/>  
               <Scroll>
-                <CardList robots={roboFilter}/>
+                <ErrorBoundry>
+                  <CardList robots={roboFilter}/>
+                </ErrorBoundry>
               </Scroll>
              
             </div>
